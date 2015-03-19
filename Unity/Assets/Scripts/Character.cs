e52a72645c3da2fcfe8097ca57f4d5ee338989c1
KevinRoussel <roussel@intechinfo.fr>
2015-03-19 12:43:59 +0100

    Sav tranquille, pour le moment ça marche bof pour l'animation

--- Unity/Assets/Scripts/Character.cs
+++ Unity/Assets/Scripts/Character.cs
@@ -14,15 +14,32 @@ public class Character : MonoBehaviour
 	private int _life;
 	private int _lookaroundcount;
 	private List<Vector3> _vecteursAttaques;
+	private Animation anims;
+	
+	private float _lastAttack;
+	public float _attackspeed;
 
     public float Speed
     {
         set { _speed = value; }
+        get{ return _speed; }
     }
+    
+    
 
 	// Use this for initialization
 	void Start()
 	{
+		_lastAttack=0;
+		// Gestion des mauvaises init
+		if(_attackspeed == 0) {_attackspeed = 2f;}
+		
+		// Animation Manager
+		anims = this.GetComponent<Animation>();
+		if(anims == null) {
+			anims = this.GetComponentInChildren<Animation>();
+		}
+	
 		_lookaroundcount = 300;
 		_life =3;
         _hit = new RaycastHit();
@@ -59,79 +76,76 @@ public class Character : MonoBehaviour
     {
 		// Gravité
         this.GetComponent<CharacterController>().Move( Vector3.down );
+        
 	}
 	
-	// Testing _motion code
-	public void DontMove() 
-    {
-        Animation list = _controller.gameObject.GetComponentInChildren<Animation>();
-		if(list) 
-        {
-            list.CrossFade( "idle" );
-		}	
-	}
+	
+	
 	public void Move(Vector3 direction) 
 	{
+		if(!isAttacking()) {
 		
-		// Si on doit réellement bouger
-		if( direction != Vector3.zero )
-        {
-			// On joue l'anim walk si elle existe
-            Animation list = _controller.gameObject.GetComponentInChildren<Animation>();
-			if(list)
-            {
-                list.CrossFade( "walk" );
-			}
-
-            _controller.Move( direction * _speed * Time.deltaTime );
-			_controller.transform.rotation = Quaternion.Slerp( _controller.transform.rotation, Quaternion.LookRotation( direction ), _speedRotation * Time.deltaTime );
-		}
-		else
-        {
-			// Joue l'anime idle
-			Animation anim = _controller.gameObject.GetComponentInChildren<Animation>();
-			if(anim)
-            {
-                anim.CrossFadeQueued( "idle" );
-			}	
-			// Gestion du mouvement lookaround quand on reste static un petit moment
-			_lookaroundcount --;
-			if(_lookaroundcount < 0)
-            {
-                _lookaroundcount = 300;
-                anim.CrossFadeQueued( "look_around", 1f, QueueMode.PlayNow );
-			}	
-		}
-	}
-
-	public void Attack() 
-	{
-		
-		foreach(Vector3 vec in _vecteursAttaques) {
-			Debug.DrawRay(_controller.transform.position, _controller.transform.TransformDirection(vec),Color.yellow,1.0f);
-			
-	        if ( Physics.Raycast( _controller.transform.position, _controller.transform.TransformDirection( vec ), out _hit ) )
-	        {
-	        	//DEBUG 
-				_adv= null;
-	            _adv = _hit.transform.GetComponent<Character>();
-				Debug.Log (_hit.collider.name);
+			// Si on doit réellement bouger
+			if( direction != Vector3.zero )
+			{
+				// On joue l'anim walk 
+				AnimationManager("walk");
 				
-				if(_adv != null) 
-	            {
-					Debug.Log ("touch ! et attaque");
+				
+				_controller.Move( direction * _speed * Time.deltaTime );
+				_controller.transform.rotation = Quaternion.Slerp( _controller.transform.rotation, Quaternion.LookRotation( direction ), _speedRotation * Time.deltaTime );
+			}
+			else
+			{
+				if(!isAttacking()) AnimationManager("idle");
+				
+				// Gestion du mouvement lookaround quand on reste static un petit moment
+				_lookaroundcount --;
+				if(_lookaroundcount < 0)
+				{
+					_lookaroundcount = 300;
+					AnimationManager("look_around");
+				}	
+			}
+		
+		}
+		
+	}
+	private bool isAttacking() {
+		return _lastAttack+_attackspeed < Time.fixedTime;
+	}
+	public void Attack() 
+	{	
+		// Gestion du tick
+		
+		if(_lastAttack+_attackspeed > Time.fixedTime) {
+			Debug.Log ("ATTAQUE");
+			_lastAttack = Time.fixedTime;	// MaJ
+			
+			// Qui on attaque
+			foreach(Vector3 vec in _vecteursAttaques) {
+				Debug.DrawRay(_controller.transform.position, _controller.transform.TransformDirection(vec),Color.yellow,1.0f);
+				
+				if ( Physics.Raycast( _controller.transform.position, _controller.transform.TransformDirection( vec ), out _hit ) )
+				{
+					//DEBUG 
+					_adv= null;
+					_adv = _hit.transform.GetComponent<Character>();
 					
-	                _adv.takeDamage();
+					if(_adv != null) 
+					{						
+						_adv.takeDamage();
+					}
 				}
 			}
+			
+			// Dans tout les cas on met l'anim d'attaque
+			AnimationManager("bim");
+			
+			
 		}
 		
-		// Dans tout les cas on met l'anim d'attaque
-		Animation anim = _controller.gameObject.GetComponentInChildren<Animation>();
-		if(anim) {
-			//anim.CrossFadeQueued("bim",1f, QueueMode.PlayNow);
-			anim.Play ("bim");
-		}	
+		
 
 	}
 
@@ -147,12 +161,26 @@ public class Character : MonoBehaviour
 		}
 		this._life -= 1;
 
-        Debug.Log( "Je suis " + this.name + " et je viens de prendre des dégats :-(" );
-        Debug.Log( "Vie restante : " + this._life );
-
         if ( this._life <= 0 )
         {
             GameObject.Destroy( this.gameObject, 0.5f );
 		}
 	}
+	
+	private void AnimationManager(string anim) {
+		// Si notre gestionnaire d'anim existe bel et bien :
+		if(anims != null) {
+		
+			anims.CrossFade(anim);
+			
+		}
+	}
+	
+	
+	public void ParticuleManager() {
+	
+	
+	
+	}
+	
 }
