38bbbb54e169ec4b861fd3c9f1bb072c4d31e764
Aymeric <richard.aymeric.92@gmail.com>
2015-04-09 23:03:01 +0200

    Affichage correct du joystick virtuel.

--- Unity/Assets/Scripts/MoveCamera.cs
+++ Unity/Assets/Scripts/MoveCamera.cs
@@ -17,15 +17,15 @@ public class MoveCamera : MonoBehaviour
 
 	void Update () 
     {
-    	if(cible != null) {
-    	
+    	if(cible != null)
+		{
 			Vector3 mouvement = this.transform.position;
+
 			mouvement.x = cible.transform.position.x;
 			mouvement.z = cible.transform.position.z - 10;
 			mouvement.y = cible.transform.position.y + 15;
 			
 			this.transform.position = mouvement;
-    	
     	}
 		
 
