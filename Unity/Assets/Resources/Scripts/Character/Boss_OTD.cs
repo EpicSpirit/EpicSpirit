7551b8965eb1f956c1576f8c5bccfdba11e0c00e
Aymeric <richard.aymeric.92@gmail.com>
2015-04-14 13:43:00 +0200

    Correction de l'instantiation d'un BadBoy

--- Unity/Assets/Resources/Scripts/Character/Boss_OTD.cs
+++ Unity/Assets/Resources/Scripts/Character/Boss_OTD.cs
@@ -42,8 +42,8 @@ public class Boss_OTD : Ennemi {
 		
 		position.x += _randomGenerator.Next(1,5);
 		position.z += _randomGenerator.Next(1,5);
-		
-		GameObject obj = Instantiate((UnityEngine.Object)UnityEngine.Resources.Load<UnityEngine.Object>("BadBoy/Ennemi 1"),position, this.transform.rotation) as GameObject;
+
+        GameObject obj = Instantiate( (UnityEngine.Object)UnityEngine.Resources.Load<UnityEngine.Object>( "BadBoy/Prefab_BadBoy" ), position, this.transform.rotation ) as GameObject;
 		Ennemi ennemi = obj.GetComponent<Ennemi>();
 		ennemi.Invokation();	
 	}
