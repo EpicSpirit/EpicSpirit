9684205d45d2f821421c081a1554e4a18a603375
Aymeric <richard.aymeric.92@gmail.com>
2015-04-28 11:07:16 +0200

    Début du menu + desindexation des .orig

---
+++ Unity/Assets/Resources/Scripts/MainMenu/MouseGesture.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseGesture : MonoBehaviour 
+{
+    //MeshRenderer text;
+	void Start () 
+    {
+       // text = GetComponent<MeshRenderer>();
+	}
+	
+	void Update () 
+    {
+	   //text.material.color = Color.green;
+	}
+    public void OnMouseEnter()
+    {
+        Debug.Log("aaaaaaa");
+    }
+    public void OnMouseUp()
+    {
+        Debug.Log( "bbbbb" );
+        Application.LoadLevel( "Scene1" );
+    }
+}
