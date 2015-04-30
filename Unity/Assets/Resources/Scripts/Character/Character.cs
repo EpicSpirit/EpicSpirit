153d719ac509899d6daeda47abc375485eff7dec
KevinRoussel <roussel@intechinfo.fr>
2015-04-30 09:45:56 +0200

    StateManager Fait. PLus qu'a l'utiliser proprement

--- Unity/Assets/Resources/Scripts/Character/Character.cs
+++ Unity/Assets/Resources/Scripts/Character/Character.cs
@@ -293,5 +293,46 @@ namespace EpicSpirit.Game
             return counter;
         }
 
+    static int[] _multiplyDeBruijnBitPosition = 
+				{ 
+					0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8, 
+					31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9 
+				};
+
+    #region LogCalul_ThanksToCKCORE
+    /// <summary>
+    /// Compute the Log2 (logarithm base 2) of a given number.
+    /// </summary>
+    /// <param name="v">Integer to compute</param>
+    /// <returns>Log2 of the given integer</returns>
+    [CLSCompliant( false )]
+    static public int Log2 ( UInt32 v )
+    {
+        unchecked
+        {
+            v |= v >> 1;
+            v |= v >> 2;
+            v |= v >> 4;
+            v |= v >> 8;
+            v |= v >> 16;
+            v = ( v >> 1 ) + 1;
+            return _multiplyDeBruijnBitPosition [( ( v * 0x077CB531U ) ) >> 27];
+        }
     }
+
+    /// <summary>
+    /// Compute the Log2ForPower2 (logarithm base 2 power 2) of a given number.
+    /// </summary>
+    /// <param name="v">Integer to compute. It MUST be a power of 2.</param>
+    /// <returns>Result</returns>
+    [CLSCompliant( false )]
+    static public int Log2ForPower2 ( UInt32 v )
+    {
+        unchecked
+        {
+            return _multiplyDeBruijnBitPosition [( ( ( uint ) v * 0x077CB531U ) ) >> 27];
+        }
+    }
+    #endregion
+
 }
