e3dcac2ebb76aa811452757a660372e568e9baa9
KevinRoussel <roussel@intechinfo.fr>
2015-05-11 11:57:18 +0200

    Début du SaveManager

---
+++ Unity/Assets/Resources/Scripts/Competences/Competence.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Competence : MonoBehaviour {
+
+    // Fields
+    Animation _anim;
+    float _attackDuration;
+    float _delayTakeDamage;
+    int strengh;
+
+	public virtual void Start () {
+	
+	}
+	
+}
