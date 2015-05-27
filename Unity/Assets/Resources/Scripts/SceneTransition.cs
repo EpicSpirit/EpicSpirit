fc7a128b0f06ea3dd37bafd226721b4780fd4874
KevinRoussel <roussel@intechinfo.fr>
2015-05-27 16:14:15 +0200

    Remise en place des liens entre les maps, nouveau script ChangeScene

---
+++ Unity/Assets/Resources/Scripts/SceneTransition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneTransition : MonoBehaviour 
+{
+
+    public Object scene;
+
+	public void OnTriggerEnter(Collider c)
+    {
+        if(c.gameObject.name == "Spi")
+        {
+            Application.LoadLevel(scene.name);
+        }
+    }
+
+
+}
