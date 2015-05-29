0c93a871e0c57634a1ded271f4bf48a3e6b782f4
Aymeric <richard.aymeric.92@gmail.com>
2015-05-30 01:10:58 +0200

    Aller Louya ! HUD responsive et opti. Euh c'était chiant de régler la life :p

--- Unity/Assets/Resources/Scripts/UserInterface/HealthHUD.cs
+++ Unity/Assets/Resources/Scripts/UserInterface/HealthHUD.cs
@@ -6,13 +6,23 @@ namespace EpicSpirit.Game
 {
     public class HealthHUD : MonoBehaviour
     {
+		Text _hp;
+
         void Start()
         {
-
+			_hp = gameObject.AddComponent<Text>();
+			_hp.fontSize = 56;
+			_hp.fontStyle = FontStyle.Bold;
+			_hp.font = (Font)Resources.Load("UI/BLKCHCRY");
+			_hp.color = Color.red;
+			// Pourquoi il me sort 52530 à la place de 206 ? wtfff putain omg fils de pute !
+			// bon ici gros problème à résoudre. Si je mets n'importe quelle couleur il va me sortir un rouge vif.
+			// Exemple new Color(191,0,0) new Color -10,0,0) c'est pareil.
+			// Moi je veux ça exactement : (200,0,0)
         }
         void Update()
         {
-            GetComponent<Text>().text = "HP : " + GameObject.FindWithTag( "Player" ).GetComponent<Character>().Health.ToString();
+            _hp.text = "HP : " + GameObject.FindWithTag( "Player" ).GetComponent<Character>().Health.ToString();
         }
     }
 }
