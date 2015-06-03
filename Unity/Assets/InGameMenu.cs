774876c567e1d07cc88f9aef59e7ab918da449ae
Aymeric <richard.aymeric.92@gmail.com>
2015-06-03 17:09:41 +0200

    Début du menu pause + reparation du terrain

---
+++ Unity/Assets/InGameMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EpicSpirit.Game
+{
+    public class InGameMenu : MonoBehaviour
+    {
+
+        void Start()
+        {
+           // gameObject.SetActive( false );
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
