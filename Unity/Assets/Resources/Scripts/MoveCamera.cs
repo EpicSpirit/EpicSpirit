967b2c5706100a2ff3f128ae1de0b5531dec95a0
Aymeric <richard.aymeric.92@gmail.com>
2015-06-09 20:20:20 +0200

    Non fonctionnel. La caméra se rapproche quand elle detecte un mur.

--- Unity/Assets/Resources/Scripts/MoveCamera.cs
+++ Unity/Assets/Resources/Scripts/MoveCamera.cs
@@ -10,21 +10,59 @@ namespace EpicSpirit.Game
         public int x_delta;
         public int y_delta;
         public int z_delta;
+
+		Vector3 _movement;
+
+		// Zoom camera when raycast detect wall
+		float _zOffset;
+		RaycastHit _hitOrigin;
+		RaycastHit _hitCurrent;
+
         /*
          * TODO: Rendre le code plus propre et générique (gérer notament la rotation de la caméra)
          * */
+		void Start()
+		{
+			_movement = this.transform.position;
+		}
         void Update()
         {
             if ( _target != null )
             {
-                Vector3 movement = this.transform.position;
+				//new camera
+				if( Application.loadedLevelName == "forest_temple" )
+				{
+					Physics.Raycast(transform.position, _target.transform.position - transform.position, out _hitCurrent);
+					Physics.Raycast(transform.position - new Vector3( 0, 0, _zOffset + z_delta ), _target.transform.position - transform.position - new Vector3( 0, 0, _zOffset + z_delta ), out _hitOrigin);
 
-                movement.x = _target.transform.position.x+x_delta;
-                movement.z = _target.transform.position.z +z_delta;
-                movement.y = _target.transform.position.y + y_delta;
+					if(_hitCurrent.transform.name.Contains("Wall"))
+					{
+						Debug.Log(_hitCurrent.transform.name);
+						_zOffset = (_hitCurrent.transform.position.z - transform.position.z) + 1;
+						Debug.Log(_zOffset);
+						_movement.x = _target.transform.position.x + x_delta;
+						_movement.z = _target.transform.position.z + _zOffset;
+						_movement.y = _target.transform.position.y + y_delta;
+					}
+					else if(!_hitOrigin.transform.name.Contains("Wall"))
+					{
+						Debug.Log("else");
+						_movement.x = _target.transform.position.x + x_delta;
+						_movement.z = _target.transform.position.z + z_delta;
+						_movement.y = _target.transform.position.y + y_delta;
+					}
+				}
 
-                //this.transform.position = movement;
-                this.transform.position = Vector3.Lerp( this.transform.position, movement, 0.4f );
+				// Old camera
+				else 
+				{
+					_movement.x = _target.transform.position.x+x_delta;
+					_movement.z = _target.transform.position.z +z_delta;
+					_movement.y = _target.transform.position.y + y_delta;
+				}
+			
+				this.transform.position = Vector3.Lerp( this.transform.position, _movement, 0.4f );
+
             }
 
 
