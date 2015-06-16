0313106a6d5801957698be71d4cfa007d8fec3b4
KevinRoussel <roussel@intechinfo.fr>
2015-06-16 11:59:36 +0200

    Ajout d'une classe d'optimisation

---
+++ Unity/Assets/OnInvisibleOptimization.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EpicSpirit.Game
+{
+    public class OnInvisibleOptimization : MonoBehaviour
+    {
+        public MonoBehaviour _monoBehaviour;
+        public static OnInvisibleOptimization Initialize ( MonoBehaviour monoBehaviour )
+        {
+            OnInvisibleOptimization addInRenderer=monoBehaviour.GetComponentInChildren<Renderer>().gameObject.AddComponent<OnInvisibleOptimization>();
+            addInRenderer._monoBehaviour = monoBehaviour;
+            return addInRenderer;
+        }
+
+        public void OnBecameVisible ()
+        {
+            _monoBehaviour.enabled = true;
+        }
+        public void OnBecameInvisible ()
+        {
+            _monoBehaviour.enabled = false;
+        }
+    }
+}
