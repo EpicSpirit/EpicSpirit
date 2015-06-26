bb75d0d21524a1520a8321da20aaa314c8acbae2
Aymeric <richard.aymeric.92@gmail.com>
2015-06-26 15:55:27 +0200

    Switches avec plein d'options. Plusieurs switches possible.

---
+++ Unity/Assets/Resources/Scripts/Character/Items/TempleSwitchPoleSpawnEnemies.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace EpicSpirit.Game
+{
+    public class TempleSwitchPoleSpawnEnemies : TempleSwitchPole
+    {
+        [SerializeField]
+        List<CinematicSpawnPoint> _spawnPoints;
+
+        public List<CinematicSpawnPoint> SpawnPoints
+        {
+            get { return _spawnPoints; }
+            set { _spawnPoints = value; }
+        }
+
+        public void Awake()
+        {
+            if ( SpawnPoints.Count == 0 )
+            {
+                foreach ( CinematicSpawnPoint spawnPoint in GetComponentsInChildren<CinematicSpawnPoint>() )
+                {
+                    Debug.Log( "ajout" );
+                    SpawnPoints.Add( spawnPoint );
+                }
+            }
+        }
+
+        internal override void takeDamage( int force )
+        {
+         
+            foreach ( CinematicSpawnPoint spawnPoint in SpawnPoints )
+            {
+                spawnPoint.Spawn();
+                Debug.Log( "pese" );
+            }
+
+            base.takeDamage( force );
+        }
+    }
+}
---
+++ Unity/Assets/Resources/Scripts/Character/Items/TempleSwitchStone.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace EpicSpirit.Game
+{
+    public class TempleSwitchStone : TempleSwitch
+    {
+        internal override void takeDamage( int force )
+        {
+            if ( _isOn == false )
+            {
+                _isOn = true;
+                transform.Rotate( new Vector3( 90, 0, 0 ) );
+            }
+            else
+            {
+                _isOn = false;
+                transform.Rotate( new Vector3( -90, 0, 0 ) );
+            }
+
+            foreach ( GameObject gate in Gates )
+            {
+                if ( gate.activeSelf )
+                {
+                    gate.SetActive( false );
+                }
+                else
+                {
+                    gate.SetActive( true );
+                }
+            }
+            base.takeDamage( force );
+        }
+    }
+}
+
