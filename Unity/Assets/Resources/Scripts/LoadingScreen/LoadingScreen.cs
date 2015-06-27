f8ba73581f1e87d5f78d45ae79bf2bd4ce779c03
KevinRoussel <roussel@intechinfo.fr>
2015-06-27 19:40:20 +0200

    Ajout de la transition du temple, ajout de colliders

--- Unity/Assets/Resources/Scripts/LoadingScreen/LoadingScreen.cs
+++ Unity/Assets/Resources/Scripts/LoadingScreen/LoadingScreen.cs
@@ -34,12 +34,14 @@ public class LoadingScreen : MonoBehaviour
         set { _loadingText.text = value; }
     }
      * */
-
+    static bool init=false;
 	void Awake () 
     {
+        
+        init = true;
         // Load image and text into children
-        _loadingImage = transform.FindChild("Background").gameObject.AddComponent<Image>();
-        _loadingText = transform.FindChild("Text").gameObject.AddComponent<Text>();
+        _loadingImage = transform.FindChild( "Background" ).gameObject.AddComponent<Image>();
+        _loadingText = transform.FindChild( "Text" ).gameObject.AddComponent<Text>();
 
         // Load an image if specified. If not, use black background
         if ( _loadingSprite != null )
@@ -50,11 +52,12 @@ public class LoadingScreen : MonoBehaviour
         {
             _loadingImage.color = Color.black;
         }
+        _loadingImage.color = new Color( 0, 0, 0, 0 );
 
         // Text properties
         _loadingText.fontStyle = FontStyle.Italic;
-        _loadingText.font = (Font)Resources.Load( "UI/BLKCHCRY" );
-        _loadingText.color = Color.white;
+        _loadingText.font = ( Font ) Resources.Load( "UI/BLKCHCRY" );
+        _loadingText.color = new Color( 1, 1, 1, 0 );
         _loadingText.alignment = TextAnchor.MiddleCenter;
         _loadingText.resizeTextForBestFit = true;
         _loadingText.resizeTextMaxSize = 120;
@@ -70,8 +73,10 @@ public class LoadingScreen : MonoBehaviour
             _loadingText.text = "Loading...";
         }
 
-        DontDestroyOnLoad( this );
+        DontDestroyOnLoad( this.gameObject );
         hide();
+        
+        
 	}
     void Update()
     {
@@ -79,23 +84,62 @@ public class LoadingScreen : MonoBehaviour
         {
             show();
         }
-            
+        /*   
         else
         {
             hide();
         }
+         * */
             
 
     }
     public void show()
     {
+        var pas = 0.05f;
         _loadingImage.enabled = true;
         _loadingText.enabled = true;
+        
+        if(_loadingImage.color.a != 1)
+        {
+            var c = _loadingImage.color;
+            
+            if ( c.a + pas > 1 ) _loadingImage.color = new Color( c.r, c.g, c.b, 1 );
+            else _loadingImage.color = new Color( c.r, c.g, c.b, c.a + pas );
+
+            c = _loadingText.color;
+            if ( c.a + pas > 1 ) _loadingText.color = new Color( c.r, c.g, c.b, 1 );
+            else _loadingText.color = new Color( c.r, c.g, c.b, c.a + pas );
+            
+        }
+        if(_loadingImage.color.a == 1)
+        {
+            hide();
+        }
+        else
+        { 
+            Invoke("show",0.1f);
+        }
+
     }
 
     public void hide()
     {
+        var pas = 0.05f;
+
         _loadingImage.enabled = false;
         _loadingText.enabled = false;
+
+        if ( _loadingImage.color.a != 0 )
+        {
+            var c = _loadingImage.color;
+            if ( c.a + pas < 0 ) _loadingImage.color = new Color( c.r, c.g, c.b, 0 );
+            else _loadingImage.color = new Color( c.r, c.g, c.b, c.a - pas );
+
+            c = _loadingText.color;
+            if ( c.a + pas < 0 ) _loadingText.color = new Color( c.r, c.g, c.b, 0 );
+            else _loadingText.color = new Color( c.r, c.g, c.b, c.a - pas );
+
+        }
+        
     }
 }
