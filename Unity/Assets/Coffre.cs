766fe9b7df40b59278bd12ce5a6a3e8f5fe597a8
KevinRoussel <roussel@intechinfo.fr>
2015-07-02 10:44:34 +0200

    Ajout de la classe Coffre et ajout d'une autre méthode Loot plus complète (avec position d'apparition du loot et la chance que l'on a pour looter)

---
+++ Unity/Assets/Coffre.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace EpicSpirit.Game
+{
+    public class Coffre : Enemy
+    {
+        public GameObject loot;
+
+        public override void Awake ()
+        {
+            base.Awake();
+        }
+
+        internal override void takeDamage ( int force )
+        {
+            AnimationManager( "open" );
+            if ( loot != null )
+                Loot( loot, this.transform.TransformDirection( Vector3.forward ), 100 );
+            else Debug.Log("Pas d'objet de loot configuré sur le coffre "+this.name+" ... ");
+        } 
+        
+    }
+}
--- Unity/Assets/Resources/Scripts/Character/Enemy.cs
+++ Unity/Assets/Resources/Scripts/Character/Enemy.cs
@@ -11,9 +11,17 @@ namespace EpicSpirit.Game
         }
         internal virtual void Loot (UnityEngine.Object item)
         {
+            Loot( item, this.transform.position, 50 );
+        }
+
+        internal virtual void Loot ( UnityEngine.Object item, Vector3 appearancePosition, int opportunity )
+        {
             var num = Random.Range( 1, 100 );
-            if ( num > 50 )
-                Instantiate( item, this.transform.position, this.transform.rotation );
+            if ( num > opportunity )
+            {
+                var instance = Instantiate( item, appearancePosition, Quaternion.Euler(Vector3.zero));
+            }
+            
         }
     }
 }
