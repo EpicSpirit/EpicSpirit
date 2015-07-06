181b0cde932a82cdf4f784ba6b46a0ccd41c9d71
Aymeric <richard.aymeric.92@gmail.com>
2015-07-06 19:54:45 +0200

    Histoire de la map d'intro

---
+++ Unity/Assets/Intro.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace EpicSpirit.Game
+{
+    public class Intro : MonoBehaviour
+    {
+        Image _imageIntro;
+        public void Awake()
+        {
+            _imageIntro = GetComponent<Image>();
+        }
+        public void Start()
+        {
+            Invoke( "Hide", 21f );
+        }
+
+        private void Hide()
+        {
+            GameObject.Destroy( _imageIntro );
+        }
+    }
+}
